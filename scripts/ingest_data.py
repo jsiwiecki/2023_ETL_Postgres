656c96cab5a051d8a2a549c9978da4bf53bdb16a
Grzywac <jsiwiecki@gmail.com>
2023-05-15 14:01:52 +0200

    Added waiting for postgres serice. Fixed ingestion

--- scripts/ingest_data.py
+++ scripts/ingest_data.py
@@ -2,7 +2,7 @@
 
 import os
 from pyspark.sql import SparkSession
-from schemas import adress_schema, customers_schema, employees_schema, products_schema, purchase_schema, representatives_schema, shops_schema
+from schemas import address_schema, customers_schema, employees_schema, products_schema, purchase_schema, representatives_schema, shops_schema
 
 
 spark = (
@@ -20,17 +20,17 @@ properties = {
 }
 
 files = [
-        "adress.json",
-        "customers.json",     
+        "customers.json",
+        "address.json",
+        "shops.json",
         "employees.json",
         "products.json",
         "purchase.json",
-        "representatives.json",
-        "shops.json"
+        "representatives.json"
     ]
 
 schemas = {
-    "adress": adress_schema,
+    "address": address_schema,
     "customers": customers_schema,
     "employees": employees_schema,
     "products": products_schema,
@@ -44,16 +44,56 @@ dataframes = {}
 
 for file in files:
     table_name = os.path.splitext(file)[0]
-    data = spark.read.option('multiline', True).schema(schemas[table_name]).json(file)
+    data = spark.read.option('multiline', True).schema(schemas[table_name]).json(f'data/{file}')
     schema = data.schema
     dataframes[table_name] = data
 
-
 dataframes['shops'].show()
-#dataframes['products'].show()
+dataframes['products'].show()
+
+products_df = dataframes['products']
+
+print('Przed zapisaem')
+
+products_df.select("*") \
+    .write.format("jdbc") \
+    .option("url", 'jdbc:postgresql://postgres:5432/example_db') \
+    .option("driver", "org.postgresql.Driver") \
+    .option("dbtable", "products") \
+    .option("user", "postgres") \
+    .option("password", 'postgres') \
+    .mode("overwrite") \
+    .save()
 
 
-#    data.write \
-#        .jdbc("jdbc:postgresql://localhost:5432/mydatabase", table_name, mode="overwrite", properties=properties)
+print("Po zapisie")
+
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname="example_db",
+    user="postgres",
+    password="postgres",
+    host="postgres",
+    port="5432"
+)
+
+
+print(f"Connection status: {conn}")
+conn.close()
+
+
+
+
+#dataframes['shops'].write \
+#    .jdbc(f"jdbc:postgresql://postgres:postgres@localhost:5432/example_db", table_name, mode="overwrite", properties=properties)
+
+
+
+#for table_name, dataframe in dataframes.items():
+#    dataframe.write \
+#        .jdbc(f"jdbc:postgresql://postgres:5432/example_db", table_name, mode="overwrite", properties=properties)
 
 spark.stop()
+
+### How to connect to postgres and save data?
